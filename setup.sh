fb9527789e1e13981a9a7fb4fc5f8f146aa7b549
sickleaf <5824061+sickleaf@users.noreply.github.com>
2020-03-04 20:54:14 +0900

    [mod] (1)add .inputrc section (2)refactoring

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-BASHRC=$(find ~/.bashrc)
+scriptDir=$(cd $(dirname $0); pwd);
+backupDir=${scriptDir}/dotfilesBK
+
+mkdir -p ${backupDir}
+
+BASHRC=$(find ${HOME}/.bashrc)
+INPUTRC=${HOME}/.inputrc
+
 checkHIST=$(grep "HISTTIMEFORMAT=" ${BASHRC}) 
+
+### if HISTTIMEFORMAT= line exists, do nothing
+### if not, append setting
 
 if [ -z "${checkHIST}" ];then
   echo '' >> ${BASHRC}
@@ -9,4 +19,15 @@ if [ -z "${checkHIST}" ];then
   echo "[DONE] HISTTIMEFORMAT (${BASHRC})"
 fi
 
+### if ~/.inputrc exists, backup ${INPUTRC} before replacing .inputrc
+
+if [ -f ${INPUTRC} ];then
+	cp -pv ${INPUTRC} ${backupDir}
+fi
+
+cp -pv ${scriptDir}/.inputrc ${INPUTRC}
+
+
+
 . ${BASHRC}
+. ${INPUTRC}
